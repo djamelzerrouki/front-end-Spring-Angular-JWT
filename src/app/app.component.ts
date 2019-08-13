2025bc291a4da584a3c772602a2188cefb7c93aa
zerrouki djamel <djameljimmizerrouki@gmail.com>
2019-08-14 00:03:21 +0200

    get categories list with catalogueService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CatalogueService} from "./catalogue.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'myproject';
+
+  private categories;
+constructor(private catService:CatalogueService){
+
+}
+  ngOnInit(): void {
+    this.getCategories();
+  }
+  private getCategories() {
+this.catService.getResource("/categories")
+  .subscribe(data=>{
+    this.categories=data;
+  },err=>{
+    console.log(err)
+  })
+  }
 }
